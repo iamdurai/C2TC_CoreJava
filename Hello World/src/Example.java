58027285ad56cb515f28945ebf838139b373cec2
ELCOT <ELCOT@Lenovo>
2022-02-16 14:52:39 +0530

    first comit

---
+++ Hello World/src/Example.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+
+/**
+ * @author ELCOT
+ *
+ */
+public class Example
+{
+	public static void main(String [] args)
+	{
+		int a=20, b=98;
+		int c=a+b;
+		System.out.println(c);
+	}
+}
